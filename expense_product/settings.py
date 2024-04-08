7c9128de9c4868982441afebbd26598767d97b93
Sean Boateng <seanboateng2@gmail.com>
2024-04-08 11:10:47 -0400

    Created database

--- expense_product/settings.py
+++ expense_product/settings.py
@@ -19,6 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -103,7 +104,7 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_TZ =False
+USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
@@ -115,7 +116,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-try:
-    from drf_jwt_backend.local_settings import *
-except ImportError:
-    pass
